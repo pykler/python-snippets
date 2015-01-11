b4e791c4c25cbd55779adb8d70fa28d3fa0b8e38
Hatem Nassrat <hnassrat@gmail.com>
2015-01-11 13:45:38 -0400

    Adding fark (Find And Replace Key) to dict functions.

--- python_snippets/dict.py
+++ python_snippets/dict.py
@@ -10,3 +10,22 @@ def dict_findall(d, k, path=[]):
         if isinstance(v, dict):
             for p in dict_findall(v, k, path+[_k]):
                 yield p
+
+def fark(d, key, search, replace):
+    '''
+    look for keys in ``d`` that match ``key`` recursively
+    replace ``search`` with ``replace`` for that value
+
+    NOTE: mutates original dict `d`, returns None
+
+    >>> d = {'a': {'file': '/usr/local/module/a.txt', 'b': {'file': '/usr/local/module/b.txt'}}}
+    >>> fark(d, 'file', '/usr', '/home/hatem')
+    >>> d == {'a': {'file': '/home/hatem/local/module/a.txt', 'b': {'file': '/home/hatem/local/module/b.txt'}}}
+    True
+    '''
+    for k,v in d.items():
+        if isinstance(v, dict):
+            fark(v, key, search, replace)
+        else:
+            if k == key:
+                d[k] = v.replace(search, replace)
